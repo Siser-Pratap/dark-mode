5786ffccea3d676e2ad7d78ec05da85805e1c776
Siser Pratap <siserinsevoc@gmail.com>
2024-09-09 05:19:39 +0530

    created react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <button className='w-8 h-8 text-blue-500'>Dark mode</button>
+    </>
+  );
+}
+
+export default App;
